6826699ab9364b7f48a7d78687162b1bcab50c77
BAOUNIMEN <benounimen91@gmail.com>
2023-05-08 15:27:27 +0200

    fixed css

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Apick from './Apick';
 function App() {
   return (
     <div className="App">
-<Pixaby />
-{/* <Apick /> */}
+      <Pixaby />
+      {/* <Apick /> */}
     </div>
   );
 }
--- src/Pixaby.js
+++ src/Pixaby.js
@@ -6,41 +6,41 @@ const Pixaby = () => {
     // var API_KEY = '36055986-bab8a116bbdfa0e8b9fd14dbb';
     const handleSubmit = (e) => {
         e.preventDefault();
-        const fetchdata = async() => {
+        const fetchdata = async () => {
             try {
                 const res = await axios.get(`https://pixabay.com/api/?key=36055986-bab8a116bbdfa0e8b9fd14dbb&q=${img}&image_type=photo&pretty=true`)
                 console.log(res)
-                setResultat(res.data.hits);            
+                setResultat(res.data.hits);
             } catch (error) {
                 console.log(error)
             }
         }
-     
-       fetchdata()
+
+        fetchdata()
     }
     return (
         <div>
             <form action="" onSubmit={handleSubmit}  >
-                <input
+                <input className='input'
                     type="text"
                     placeholder="enter pic name..."
                     value={img}
                     onChange={(e) => setImg(e.target.value)}
                 />
-                <button type="sumbit">Get photo</button>
+                <button className=' btn-primary ' type="sumbit">Get photo</button>
             </form>
-            <div className='' >
-               
-                    {
-                        resultat.map((el)=>{
-                            return(
-                                <div class="">
-                                    <img  src={el.largeImageURL}/>
-                                </div>
-                            )
-                        })
-                    }
-                
+            <div className='container' >
+
+                {
+                    resultat.map((el) => {
+                        return (
+                            <>
+                                <img  src={el.largeImageURL} />
+                            </>
+                        )
+                    })
+                }
+
             </div>
         </div>
     )
